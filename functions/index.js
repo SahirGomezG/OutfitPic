38c9e4f19749068a50bc5c2a56c76ee41d123780
SahirGomezG <m.gomez2@columbia.edu>
2020-03-27 19:08:09 -0400

    indexjs cloud functions

--- functions/index.js
+++ functions/index.js
@@ -7,17 +7,90 @@ admin.initializeApp(functions.config().firebase);
 
 exports.sendPushNotification = functions.firestore
   .document('outfitPolls/{pollId}')
-  .onCreate(event => {
-    const writeData = event.data.data();
+  .onCreate((snap, context) => {
+    const postData = snap.data();
+    const author = postData.user.name;
+    console.log('PollId:', context.params.pushId, 'Data:', postData);
     let payload = {
       notification: {
-        title: 'New Poll from: ',
-        body: writeData.user.name,
+        title: 'New OutfitPic Post',
+        body: `Hey! new Poll from ${author}, check it out.`,
         sound: 'default',
-        badge: '',
       },
     };
     let pushToken =
       'eBxLg61a3UFBmvOT9mA-MK:APA91bFJoeCUGODczo7b-KtIKcHOHYP-E4ecM7h5juKztjvW77L2fSUD-TKK_34nriiS9AcTqKTkaxJCOeU9PHfh0nr9eMPCbutNQ0rFVZzellT1EiDbbeerrO2ux9DdT7yYBiS1Iuxq';
-    admin.messaging().sendToDevice(pushToken, payload);
+    return admin
+      .messaging()
+      .sendToDevice(pushToken, payload)
+      .then(response => {
+        return console.log('Successfully sent message:', response);
+      })
+      .catch(error => {
+        return console.log('Error sending message:', error);
+      });
+  });
+
+exports.pushNotificationChat = functions.database
+  .ref('/messages/{pushId}')
+  .onCreate((snapshot, context) => {
+    // Grab the current value of what was written to the Realtime Database.
+    const message = snapshot.val();
+    const myId = context.auth.uid;
+    let nodeId = context.params.pushId;
+    let uid = nodeId.slice(28);
+    console.log(
+      'Message ID:',
+      nodeId,
+      'myId:',
+      myId,
+      'targetId:',
+      uid,
+      'Message Data:',
+      message,
+    );
+    let payload = {
+      notification: {
+        title: 'OutfitPic Chat',
+        body: 'New message from your friend ',
+        sound: 'default',
+      },
+    };
+    let pushToken =
+      'c6vZv4DeKU0msgx5oHII_g:APA91bH29ui97WbqtHhDCnYo7iIOf8iQChLJZZfdLsxka8EqjUsnbCqdLMcnOvT5Uir4SfEPPKZ5o6_sNSegj3lcx6l9GHbNhV062_47G9P3Gc7VagaYfQ66hirBHn9QaTyNRkPCVSbI';
+    return admin
+      .messaging()
+      .sendToDevice(pushToken, payload)
+      .then(response => {
+        return console.log('Successfully sent message:', response);
+      })
+      .catch(error => {
+        return console.log('Error sending message:', error);
+      });
+  });
+
+exports.sendNewFollowerNotification = functions.firestore
+  .document('users/{uid}/followers/{followerId}')
+  .onCreate((snap, context) => {
+    const followerData = snap.data();
+    let myId = context.params.uid;
+    //let followerId = context.params.followerId;
+    const followerName = followerData.name;
+    let payload = {
+      notification: {
+        title: 'Hey! You have a new follower',
+        body: `${followerName} started following you`,
+        sound: 'default',
+      },
+    };
+    console.log(payload);
+    return admin
+      .firestore()
+      .collection('users')
+      .doc(myId)
+      .get()
+      .then(doc => {
+        pushToken = doc.data().pushToken;
+        return admin.messaging().sendToDevice(pushToken, payload);
+      });
   });
